acd517f333c47d6d1169e9d96a65853b0a7d8158
Panithan Kumthonchaiphimol <panithan@devdronemapper.com>
2024-02-26 11:07:08 +0700

    Config database connect_5

--- utilities/poolConfig.js
+++ utilities/poolConfig.js
@@ -1,53 +1,5 @@
 const { Pool } = require('pg');
 
-const {
-    POSTGRES_HOST,
-    POSTGRES_PORT,
-    POSTGRES_DATABASE,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-} = process.env;
-
-
-
-let pool = new Pool({
-    port: POSTGRES_PORT,
-    host: POSTGRES_HOST,
-    database: POSTGRES_DATABASE,
-    password: POSTGRES_PASSWORD,
-    user: POSTGRES_USER,
-    port: POSTGRES_PORT,
-});
-
-// if (process.env.NODE_ENV === 'production') {
-//     pool = new Pool({
-//         host: POSTGRES_HOST,
-//         port: POSTGRES_PORT,
-//         database: POSTGRES_DATABASE,
-//         user: POSTGRES_USER,
-//         password: POSTGRES_PASSWORD,
-//     });
-// } else {
-//     pool = new Pool({
-//         port: 5432,
-//         host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
-//         database: 'ddm_iso',
-//         password: 'remote',
-//         user: 'postgres',
-//         port: 5432,
-//     });
-// }
-
-pool.connect((err) => {
-    if (err) throw err
-    console.log('Connect data base SS')
-})
-
-module.exports = { pool };
-
-
-// const { Pool } = require('pg');
-
 // const {
 //     POSTGRES_HOST,
 //     POSTGRES_PORT,
@@ -56,15 +8,22 @@ module.exports = { pool };
 //     POSTGRES_PASSWORD,
 // } = process.env;
 
-// const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`
 
-// const pool = new Pool({
-//     connectionString,
-// });
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'remote',
+    host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
+    port: 5432,
+    database: 'ddm_iso'
+});
+
 
 // pool.connect((err) => {
-//     if (err) throw err;
-//     console.log('Connected to database SS');
-// });
+//     if (err) throw err
+//     console.log('Connect data base SS')
+// })
 
-// module.exports = { pool };
+module.exports = { pool };
+
+
