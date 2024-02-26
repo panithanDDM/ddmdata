019837c63d15dd8ed02e8af26c213f66e5471a24
Panithan Kumthonchaiphimol <panithan@devdronemapper.com>
2024-02-26 10:42:36 +0700

    Config database connect_3

--- utilities/poolConfig.js
+++ utilities/poolConfig.js
@@ -1,3 +1,42 @@
+// const { Pool } = require('pg');
+
+// const {
+//     POSTGRES_HOST,
+//     POSTGRES_PORT,
+//     POSTGRES_DATABASE,
+//     POSTGRES_USER,
+//     POSTGRES_PASSWORD,
+// } = process.env;
+
+// let pool;
+
+// if (process.env.NODE_ENV === 'production') {
+//     pool = new Pool({
+//         host: POSTGRES_HOST,
+//         port: POSTGRES_PORT,
+//         database: POSTGRES_DATABASE,
+//         user: POSTGRES_USER,
+//         password: POSTGRES_PASSWORD,
+//     });
+// } else {
+//     pool = new Pool({
+//         port: 5432,
+//         host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
+//         database: 'ddm_iso',
+//         password: 'remote',
+//         user: 'postgres',
+//         port: 5432,
+//     });
+// }
+
+// pool.connect((err) => {
+//     if (err) throw err
+//     console.log('Connect data base SS')
+// })
+
+// module.exports = { pool };
+
+
 const { Pool } = require('pg');
 
 const {
@@ -8,30 +47,15 @@ const {
     POSTGRES_PASSWORD,
 } = process.env;
 
-let pool;
+const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`
 
-if (process.env.NODE_ENV === 'production') {
-    pool = new Pool({
-        host: POSTGRES_HOST,
-        port: POSTGRES_PORT,
-        database: POSTGRES_DATABASE,
-        user: POSTGRES_USER,
-        password: POSTGRES_PASSWORD,
-    });
-} else {
-    pool = new Pool({
-        port: 5432,
-        host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
-        database: 'ddm_iso',
-        password: 'remote',
-        user: 'postgres',
-        port: 5432,
-    });
-}
+const pool = new Pool({
+    connectionString,
+});
 
 pool.connect((err) => {
-    if (err) throw err
-    console.log('Connect data base SS')
-})
+    if (err) throw err;
+    console.log('Connected to database SS');
+});
 
 module.exports = { pool };
