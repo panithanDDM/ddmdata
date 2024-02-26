32a50d1081d690f0eab263808b50a73b64ed8eb8
Panithan Kumthonchaiphimol <panithan@devdronemapper.com>
2024-02-26 12:10:21 +0700

    Config database connect_7

--- utilities/poolConfig.js
+++ utilities/poolConfig.js
@@ -1,37 +1,38 @@
 const { Pool } = require('pg');
-
-// const {
-//     POSTGRES_HOST,
-//     POSTGRES_PORT,
-//     POSTGRES_DATABASE,
-//     POSTGRES_USER,
-//     POSTGRES_PASSWORD,
-// } = process.env;
+require('dotenv').config();
 
 
+// let pool;
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     password: 'remote',
-//     host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
-//     port: 5432,
-//     database: 'ddm_iso'
-// });
+// if (process.env.NODE_ENV === 'local') {
+//     pool = new Pool({
+//         user: 'postgres',
+//         password: 'remote',
+//         host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
+//         port: 5432,
+//         database: 'ddm_iso'
+//     });
+// } else {
+//     pool = new Pool({
+//         user: process.env.PG_USER,
+//         password: process.env.PG_PASSWORD,
+//         host: process.env.PG_HOST,
+//         port: process.env.PG_PORT,
+//         database: process.env.PG_DATABASE,
+//         ssl: true,
+//     });
+// }
 
 const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE
-});
-
-
-// pool.connect((err) => {
-//     if (err) throw err
-//     console.log('Connect data base SS')
-// })
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    ssl: true,
+})
 
 module.exports = { pool };
+
 
 
