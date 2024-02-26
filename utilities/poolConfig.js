601e5cf895dbd26c97bac6aa4f49e43e9082e21d
Panithan Kumthonchaiphimol <panithan@devdronemapper.com>
2024-02-26 13:00:55 +0700

    Config pool_1

--- utilities/poolConfig.js
+++ utilities/poolConfig.js
@@ -24,12 +24,11 @@ require('dotenv').config();
 // }
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    ssl: true,
+    user: 'postgres',
+    password: 'remote',
+    host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
+    port: 5432,
+    database: 'ddm_iso'
 })
 
 module.exports = { pool };
