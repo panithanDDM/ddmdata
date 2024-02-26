e3b8ce1b44510163462a6cc11e5cd068966b94ab
Panithan Kumthonchaiphimol <panithan@devdronemapper.com>
2024-02-26 11:52:34 +0700

    Config database connect_6

--- utilities/poolConfig.js
+++ utilities/poolConfig.js
@@ -10,12 +10,20 @@ const { Pool } = require('pg');
 
 
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'remote',
+//     host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
+//     port: 5432,
+//     database: 'ddm_iso'
+// });
+
 const pool = new Pool({
-    user: 'postgres',
-    password: 'remote',
-    host: '2403:6200:8917:dfb9:4c55:bbf5:64ef:2631',
-    port: 5432,
-    database: 'ddm_iso'
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE
 });
 
 
